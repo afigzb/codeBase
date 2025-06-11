338125ba4a5a9c401539c0f057177b595fc25279
aliyun0056826799-Pa6Fg <361747532@qq.com>
2025-06-11 12:15:40 +0800

    把h5模式改成哈希模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '../components/homePage/HomePage.vue'
 import DesignPage from '../components/Designpage/DesignPage.vue'
 import ComponentsPage from '../components/ComponentsPage/ComponentsPage.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
